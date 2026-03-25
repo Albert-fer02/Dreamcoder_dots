7ba9f889bc0aae8356e49a28333cb56d3e76aba8
dreamcoder08 <tu-email@example.com>
2026-03-24 21:43:13 -0500

    fix: use --kitty flag for fastfetch

--- scripts/fastfetch.sh
+++ scripts/fastfetch.sh
@@ -3,4 +3,4 @@
 IMG_DIR="$HOME/.config/dreamcoder"
 IMGS=(Dreamcoder01.jpg Dreamcoder02.jpg Dreamcoder03.jpg Dreamcoder04.jpg Dreamcoder05.jpg Dreamcoder06.jpg Dreamcoder07.jpg Dreamcoder08.jpg Dreamcoder09.jpg)
 RANDOM_IMG="${IMGS[$((RANDOM % 9))]}"
-fastfetch -l "$IMG_DIR/$RANDOM_IMG"
+fastfetch --kitty "$IMG_DIR/$RANDOM_IMG"
